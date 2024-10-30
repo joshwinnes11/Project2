68567864b3a6f85bb6f608d5bbdeef47e8807770
Josh Winnes <joshwinnes@icloud.com>
2024-10-30 18:31:10 -0500

    cleaned everything up

--- gpx_kmz_util.py
+++ gpx_kmz_util.py
@@ -3,6 +3,8 @@ from fastkml import kml
 import gpxpy
 import pandas as pd
 
+### Functions for reading in the data
+
 def gpx_to_df(file_name):
     # Load your GPX file
     gpx_file = open(file_name, 'r')
@@ -70,6 +72,8 @@ def parse_kml_to_dataframe(kml_data):
     return df
 
 
+### Preprocessing Functions
+
 def data_preprocessing(gpx_file_name, kmz_file_path):
     
     gpx_data = gpx_to_df(gpx_file_name)
@@ -96,6 +100,82 @@ def data_preprocessing(gpx_file_name, kmz_file_path):
     gpx_data.fillna(method='ffill', axis=0, inplace=True)
     gpx_data.fillna(value = 'pre_lift_1', inplace = True)
     
+    gpx_data = get_dist_time(gpx_data)
+    
     return gpx_data, kmz_data
+
+from haversine import haversine
+
+def get_dist_time(df):
+    # Shift latitude and longitude columns for previous points
+    df.drop_duplicates()
+    
+    df['Latitude_Shifted'] = df['Latitude'].shift()
+    df['Longitude_Shifted'] = df['Longitude'].shift()
+
+    # Calculate distances
+    df['Distance'] = df.apply(
+        lambda row: haversine(
+            (row['Latitude'], row['Longitude']),
+            (row['Latitude_Shifted'], row['Longitude_Shifted'])
+        ) * 1000 if not pd.isna(row['Latitude_Shifted']) else 0,
+        axis=1
+    )
+
+    # Calculate time differences
+    df['Time_Diff'] = df['Time'].diff().dt.total_seconds()
+
+    # Calculate speeds
+    df['Speed'] = df['Distance'] / df['Time_Diff']
+    df['Speed'].fillna(0, inplace=True)  # Set speed to 0 for the first row
+    df['Speed_MPH'] = df['Speed']*2.2369362920544
+    df['Time_Diff'].fillna(0,inplace=True)
+
+    # Drop the temporary shifted columns
+    df.drop(['Latitude_Shifted', 'Longitude_Shifted'], axis=1, inplace=True)
+    
+    df['Time'] = pd.to_datetime(df['Time'], errors = 'coerce').dt.tz_localize(None)
+    df.dropna(subset=['Time'], inplace = True)
+    
+    df = df[df['Name']!='pre_lift_1']
+    df = df[df['Speed']<45]
+
+    return df
+
+### Functions for adding things to and plotting the maps
+
+import folium
+def add_staypionts(df_stay_location, foliumMap):
+    
+    df_stay_location['datetime'] = df_stay_location['datetime'].dt.tz_localize(None)
+    
+    for lat, lng, datetime, leave_time in zip(df_stay_location['lat'], df_stay_location['lng'], df_stay_location['datetime'], df_stay_location['leaving_datetime']):
+        folium.CircleMarker(
+        location=[lat, lng],
+        tooltip = leave_time-datetime,
+        radius=10,
+        weight=2,
+        color='red'  # Use a fixed color since we're focusing on one day
+    ).add_to(foliumMap)
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl 
+def add_speed_heatmap(df_gpx, foliumMap):
+    
+    df = df_gpx.drop(labels=['Time'], axis=1)
+
+    min_speed, max_speed = df['Speed'].min(), df['Speed'].max()
+    norm = plt.Normalize(vmin=min_speed, vmax=max_speed)
+    colormap = plt.cm.get_cmap("inferno")
+
+    for idx, row in df.iterrows():
+        folium.CircleMarker(
+            location=(row['Latitude'], row['Longitude']),
+            radius=2,
+            color=mpl.colors.to_hex(colormap(norm(row['Speed']))),  # Convert to hex for folium
+            fill=True,
+            fill_color=mpl.colors.to_hex(colormap(norm(row['Speed']))),
+            fill_opacity=0.2,
+        ).add_to(foliumMap)
 
 
